29458444989c12f604ff05126c773708cdab1c4c
willemmulder <willemmulder@gmail.com>
2013-06-17 21:16:29 +0200

    First commit!

---
+++ miniRequire.js
@@ -0,0 +1,19 @@
+var require = (function() {
+	var returnFunction = function(names,callback) {
+		var returnInstances = [];
+		for(var a = 0; a < names.length; a++) {
+			returnInstances.push(require.i[names[a]]);
+		}
+		callback.apply(null, returnInstances);
+	};
+	returnFunction.i = {};
+	return returnFunction;
+})();
+
+// instance should be a function
+var define = function(name,requires,instance) {
+	require(requires, function() {
+		require.i[name] = instance.apply(null, Array.prototype.slice.call(arguments,0));
+	});
+};
+define.amd = true;
